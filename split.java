bb41e879e40c7956c46c60439f757adb1ca8ceaa
JL-20191210 <534457091@qq.com>
2024-05-01 15:36:57 +0800

    新代码

---
+++ split.java
@@ -0,0 +1,7 @@
+public class split{
+    public static void main(String[] args) {
+        String name = "hahahhahha world";
+        String x = name.substring(0,5);
+        System.out.println(x);
+    }
+}
